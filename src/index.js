f7faaefb6054aa91db2980ce3b49712bc46bbc3e
xujing <13522154894@163.com>
2019-04-04 17:57:36 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-alert('Mobile-console');
+class MobileConsole{
+    constructor(){
+        this.list = {}
+    }
+    set(key,value){
+        this.list[key] = value
+    }
+    get(key){
+        return this.list[key]
+    }
+}
+window.MobileConsole = MobileConsole
